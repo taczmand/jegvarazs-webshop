550152fcf92808e22120647c66192a14b8113700
Taczman Dávid <taczman.david@biotechusa.com>
2025-05-17 08:35:02 +0200

    A css-eket js kell importálni, hogy ne abszolút hivatkozás legyen

--- resources/js/shop.js
+++ resources/js/shop.js
@@ -1,3 +1,10 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "../css/elegant-icons.css";
+import "../css/nice-select.css";
+import "../css/jquery-ui.min.css";
+import "../css/owl.carousel.min.css";
+import "../css/slicknav.min.css";
+
 import '../js/jquery.nice-select.min.js';
 import '../js/jquery-ui.min.js';
 import '../js/jquery.slicknav.js';
