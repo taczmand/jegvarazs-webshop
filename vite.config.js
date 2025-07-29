2fcac29b04ef3d68e868a03a1f0be5e6093b8143
Taczman Dávid <taczman.david@biotechusa.com>
2025-07-29 09:10:38 +0200

    Vite config módosítás

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 
 
 export default defineConfig({
-    base: '/jegvarazsbolt.hu/build/',
+    base: '/build/',
     plugins: [
         laravel({
             input: [
