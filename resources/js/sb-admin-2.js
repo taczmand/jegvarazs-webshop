b965b7aae9b2dd2926e9d6300700caafb5d12f8f
Taczman Dávid <taczman.david@biotechusa.com>
2025-07-31 08:42:32 +0200

    Újabb próbálkozás

--- resources/js/sb-admin-2.js
+++ resources/js/sb-admin-2.js
@@ -17,11 +17,11 @@
         };*/
 
         // Toggle the side navigation when window is resized below 480px
-        if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+        /*if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
             $("body").addClass("sidebar-toggled");
             $(".sidebar").addClass("toggled");
             $('.sidebar .collapse').collapse('hide');
-        };
+        };*/
     });
 
     // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
