b0d7875f20c1bce155c799b266bbdded71bae2ac
Taczman Dávid <taczman.david@biotechusa.com>
2025-08-25 12:09:14 +0200

    Dashboard infók levétele

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,47 +9,8 @@
         <div class="container-fluid py-4">
             <h1 class="mb-4">Vezérlőpult</h1>
 
-            <div class="row g-4">
-                <!-- Összes rendelés -->
-                <div class="col-12 col-md-6 col-xl-3">
-                    <div class="card border-start border-primary border-4 shadow-sm h-100">
-                        <div class="card-body">
-                            <h6 class="card-title text-muted">Rendelések</h6>
-                            <h3 class="card-text">{{ $orderCount }}</h3>
-                        </div>
-                    </div>
-                </div>
 
-                <!-- Termékek száma -->
-                <div class="col-12 col-md-6 col-xl-3">
-                    <div class="card border-start border-success border-4 shadow-sm h-100">
-                        <div class="card-body">
-                            <h6 class="card-title text-muted">Termékek</h6>
-                            <h3 class="card-text">{{ $productCount }}</h3>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Vásárlók -->
-                <div class="col-12 col-md-6 col-xl-3">
-                    <div class="card border-start border-info border-4 shadow-sm h-100">
-                        <div class="card-body">
-                            <h6 class="card-title text-muted">Vásárlók</h6>
-                            <h3 class="card-text">{{ $customerCount }}</h3>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Árbevétel -->
-                <div class="col-12 col-md-6 col-xl-3">
-                    <div class="card border-start border-warning border-4 shadow-sm h-100">
-                        <div class="card-body">
-                            <h6 class="card-title text-muted">Árbevétel</h6>
-                            <h3 class="card-text">{{ number_format($revenue, 0, ',', ' ') }} Ft</h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     @endsection
 @endsection
