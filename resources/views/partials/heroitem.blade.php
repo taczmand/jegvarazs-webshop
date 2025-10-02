7125c79d9dac366d2eadd30dfbbc1ca2dbbef814
Taczman Dávid <taczman.david@biotechusa.com>
2025-10-02 10:41:19 +0200

    Csak a főoldalon van hero alatt termék kategória csúszka

--- resources/views/partials/heroitem.blade.php
+++ resources/views/partials/heroitem.blade.php
@@ -9,3 +9,30 @@
 
     </div>
 </div>
+<div class="row mt-3" style="width:95%; padding-left: 5%">
+    <div class="categories__slider owl-carousel">
+        @foreach($all_categories as $category)
+            @php
+                $firstProduct = $category->products->first();
+                $photo = 'static_media/no-image.jpg';
+
+                if ($firstProduct) {
+                    $mainPhoto = $firstProduct->photos->first();
+                    if ($mainPhoto) {
+                        $photo = 'storage/' . $mainPhoto->path;
+                    }
+                }
+            @endphp
+
+            <div class="col-lg-3 pr-3">
+                <div class="categories__item set-bg" data-setbg="{{ asset($photo) }}">
+                    <h5>
+                        <a href="{{ route('products.resolve', ['slugs' => $category->getFullSlug()]) }}">
+                            {{ $category->title }}
+                        </a>
+                    </h5>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
