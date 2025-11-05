f3b4c8502380b242771b3fd15f401dc739533f1d
Taczman Dávid <taczman.david@biotechusa.com>
2025-11-05 12:51:37 +0100

    Hasonló termékek képre kattintva is link

--- resources/views/pages/products/show.blade.php
+++ resources/views/pages/products/show.blade.php
@@ -215,13 +215,15 @@
                     <div class="col-lg-3 col-md-4 col-sm-6">
                         <div class="product__item">
                             <div class="product__item__pic set-bg" data-setbg="{{ asset('storage/' . $mainPhoto?->path ?? 'static_media/no-image.jpg') }}">
-                                <ul class="product__item__pic__hover">
-                                    <!--<li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-retweet"></i></a></li>-->
-                                    @auth('customer')
-                                        <li><a href="#" onclick="addToCart({{ $related_product->id }}, {{ $related_product->unit_qty }})"><i class="fa fa-shopping-cart"></i></a></li>
-                                    @endauth
-                                </ul>
+                                <a href="{{ route('products.resolve', ['slugs' => $fullSlug]) }}">
+                                    <ul class="product__item__pic__hover">
+                                        <!--<li><a href="#"><i class="fa fa-heart"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-retweet"></i></a></li>-->
+                                        @auth('customer')
+                                            <li><a href="#" onclick="addToCart({{ $related_product->id }}, {{ $related_product->unit_qty }})"><i class="fa fa-shopping-cart"></i></a></li>
+                                        @endauth
+                                    </ul>
+                                </a>
                             </div>
                             <div class="product__item__text">
                                 <h6><a href="{{ route('products.resolve', ['slugs' => $fullSlug]) }}">{{ $related_product->title }}</a></h6>
