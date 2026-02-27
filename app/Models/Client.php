73d9f601112d40891c5b029cff5d62468c98601d
Taczman Dávid <taczman.david@biotechusa.com>
2026-02-27 23:26:08 +0100

    Normalizálja az ügyfél e-mail attribútumot
    Tisztítja és egységesíti az e-mail címeket az adatbázisba mentés előtt.
    Ez biztosítja az adatok konzisztenciáját és javítja az adatintegritást azáltal, hogy eltávolítja a felesleges szóközöket, kisbetűssé alakítja a bemenetet, és null értéket állít be üres stringek vagy null bemenet esetén.

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
@@ -11,6 +12,25 @@ class Client extends Model
     use LogsActivity;
 
     protected $guarded = [];
+
+    protected function email(): Attribute
+    {
+        return Attribute::make(
+            set: function ($value) {
+                if (is_null($value)) {
+                    return null;
+                }
+
+                if (!is_string($value)) {
+                    return $value;
+                }
+
+                $value = trim($value);
+
+                return $value === '' ? null : mb_strtolower($value);
+            }
+        );
+    }
 
     public function addresses(): HasMany
     {
